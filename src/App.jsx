61765658d1964b5a60b2cfc258de09fa60c45287
rasmuslek <rasmus.lek@gmail.com>
2026-09-17 10:25:14 +0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,5 @@
-import Header from './components/Header.jsx'
-import Footer from './components/Footer.jsx'
-
+import Home from './Pages/home.jsx';
 
 export default function App() {
-  return (
-    <div>
-      <Header />
-
-      <main className="p-6">
-        <h1 className="text-2xl font-bold">
-          Welcome to my app
-        </h1>
-      </main>
-
-      <Footer />
-    </div>
-  )
+  return <Home />;
 }
